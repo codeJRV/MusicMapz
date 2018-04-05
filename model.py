545749c8359089196760a6a303abc20834a52b5f
Jibin Rajan Varghese <email.jrv@gmail.com>
2018-04-05 02:35:22 -0500

    Cleaned up the model

--- model.py
+++ model.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
+import os
 from keras import backend as K
 from keras.layers import Input, Dense
 from keras.models import Model
@@ -16,94 +17,64 @@ from keras.layers import Input, Dense
 
 
 
+def MusicTaggerCNN(weights_path, load_weights=False, input_tensor=None, num_genres = 10):
 
+    channel_axis = 1
+    time_axis = 3
 
-def MusicTaggerCNN(weights='msd', input_tensor=None):
-
-    if weights not in {'msd', None}:
-        raise ValueError('The `weights` argument should be either '
-                         '`None` (random initialization) or `msd` '
-                         '(pre-training on Million Song Dataset).')
-
-        # Determine proper input shape
-    if K.image_dim_ordering() == 'th':
-        input_shape = (1, 96, 1366)
-    else:
-        input_shape = (96, 1366, 1)
+    # Removing variations :: we are using K.image_dim_ordering() == 'th'
+    if K.image_dim_ordering() == 'tf':
+            raise RuntimeError("Please set image_dim_ordering == 'th'."
+                               "You can set it at ~/.keras/keras.json")
 
     if input_tensor is None:
-        melgram_input = Input(shape=input_shape)
+        melgram_input = Input(shape=(1, 96, 1366))
     else:
         melgram_input = Input(shape=input_tensor)
 
-    # Determine input axis
-    if K.image_dim_ordering() == 'th':
-        channel_axis = 1
-        freq_axis = 2
-        time_axis = 3
-    else:
-        channel_axis = 3
-        freq_axis = 1
-        time_axis = 2
 
     # Input block
     x = BatchNormalization(axis=time_axis, name='bn_0_freq', trainable=False)(melgram_input)
 
     # Conv block 1
-    x = Conv2D(64, (3, 3), padding="same", trainable=False, name="conv1")(x)
+    x = Convolution2D(64, (3, 3), padding="same", trainable=False, name="conv1")(x)
     x = BatchNormalization(axis=channel_axis, mode=0, name='bn1', trainable=False)(x)
     x = ELU()(x)
     x = MaxPooling2D(pool_size=(2, 4), name='pool1', trainable=False)(x)
 
     # Conv block 2
-    x = Conv2D(128, 3, 3, border_mode='same', name='conv2', trainable=False)(x)
+    x = Convolution2D(128, (3, 3), border_mode='same', name='conv2', trainable=False)(x)
     x = BatchNormalization(axis=channel_axis, mode=0, name='bn2', trainable=False)(x)
     x = ELU()(x)
     x = MaxPooling2D(pool_size=(2, 4), name='pool2')(x)
 
     # Conv block 3
-    x = Conv2D(128, 3, 3, border_mode='same', name='conv3', trainable=False)(x)
+    x = Convolution2D(128, (3, 3), border_mode='same', name='conv3', trainable=False)(x)
     x = BatchNormalization(axis=channel_axis, mode=0, name='bn3', trainable=False)(x)
     x = ELU()(x)
     x = MaxPooling2D(pool_size=(2, 4), name='pool3')(x)
 
     # Conv block 4
-    x = Conv2D(192, 3, 3, border_mode='same', name='conv4', trainable=False)(x)
+    x = Convolution2D(192, (3, 3), border_mode='same', name='conv4', trainable=False)(x)
     x = BatchNormalization(axis=channel_axis, mode=0, name='bn4', trainable=False)(x)
     x = ELU()(x)
     x = MaxPooling2D(pool_size=(3, 5), name='pool4', trainable=False)(x)
 
     # Conv block 5
-    x = Conv2D(256, 3, 3, border_mode='same', name='conv5')(x)
+    x = Convolution2D(256, (3, 3), border_mode='same', name='conv5')(x)
     x = BatchNormalization(axis=channel_axis, mode=0, name='bn5')(x)
     x = ELU()(x)
     x = MaxPooling2D(pool_size=(4, 4), name='pool5')(x)
 
     # Output
     x = Flatten(name='Flatten_1')(x)
+    x = Dense(num_genres, activation='sigmoid', name='output')(x)
 
-    if weights is None:
-        # Create model
-        x = Dense(10, activation='sigmoid', name='output')(x)
-        model = Model(melgram_input, x)
-        return model
+    model = Model(melgram_input, x)
+
+    if load_weights and os.path.isfile(weights_path):
+        model.load_weights(weights_path,by_name=True)
     else:
-        # Load input
-        x = Dense(50, activation='sigmoid', name='output')(x)
-        if K.image_dim_ordering() == 'tf':
-            raise RuntimeError("Please set image_dim_ordering == 'th'."
-                               "You can set it at ~/.keras/keras.json")
-        # Create model
-        initial_model = Model(melgram_input, x)
-        initial_model.load_weights('weights/music_tagger_cnn_weights_%s.h5' % K._BACKEND,
-                                   by_name=True)
+        raise ValueError('Invalid Path')
 
-        # Eliminate last layer
-        pop_layer(initial_model)
-
-        # Add new Dense layer
-        last = initial_model.get_layer('Flatten_1')
-        preds = (Dense(10, activation='sigmoid', name='preds'))(last.output)
-        model = Model(initial_model.input, preds)
-
-        return model
+    return model
